cce6c1d40a8804572fbfc67b47d1ba1efa38a1c0
Nambi Nachiyar <nambi2731@gmail.com>
2025-10-14 08:38:21 +0530

    remove-localhost

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ mongoose.connect(process.env.MONGO_DB)
 .then(()=>{
     console.log("Mongo DB is Connected")
 
-    app.listen(process.env.PORT,"0.0.0.0",()=>{
+    app.listen(process.env.PORT,()=>{
         console.log(`Server is running on port -> http://localhost:${process.env.PORT}`)
     })
 })
